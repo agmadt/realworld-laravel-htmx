ce9801ff9072b7daa411426d72d19deb771c6008
Agam Adhityo <adhityoagam@gmail.com>
2023-09-16 07:37:26 +0700

    Update composer lock and https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
       .tagify__input { border-radius: 4px; margin: 0; padding: 10px 12px; }
     </style>
 
-    <link href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
+    <link href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
     <meta http-equiv="refresh" content="{{ config('session.lifetime') * 60 }}">
   </head>
