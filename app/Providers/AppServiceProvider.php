ee3fa8aa22d86d87a00ba9da1e11d64eb754ce5f
Agam Adhityo <adhityoagam@gmail.com>
2023-09-18 05:56:05 +0700

    fix assets is not loaded in https on production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
