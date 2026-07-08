cf12d46111fa52d1f9dfc943cf6064bb9caaf1bb
Agam Adhityo <>
2026-07-08 07:49:43 +0700

    allow https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,4 +15,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->trustProxies('*')
+    ->create();
